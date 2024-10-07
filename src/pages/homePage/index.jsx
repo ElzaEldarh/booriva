ce16b66dd5bf014af8a00ee49bc14102bbb697ef
SedaMelkhi <31256499+SedaMelhi@users.noreply.github.com>
2024-10-07 22:16:54 +0300

    feat: просто тест

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <main></main>;
+  return (
+    <main>
+      <h1>Lorem, ipsum.</h1>
+      <p>Lorem, ipsum dolor.</p>
+    </main>
+  );
 };
 
 export default HomePage;
