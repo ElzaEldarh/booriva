973d2dec7a60c5b2b5c5c86187bc65fa8cdfe713
Arcanaest <f9d3@mail.ru>
2024-10-10 10:54:25 +0300

    feat: add banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,8 @@
+const Banner =()=>{
+    return(
+        <div className="banner-container">
+        <img className="banner-image" src="public\baner.png" alt="baner" />
+        </div>
+    )
+}
+export default Banner
--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -1,8 +1,11 @@
+import Banner from "../../components/banner/banner";
 const HomePage = () => {
+
   return (
     <main>
       <h1>Lorem, ipsum.</h1>
       <p>Lorem, ipsum dolor.</p>
+      <div><Banner/></div>
     </main>
   );
 };
